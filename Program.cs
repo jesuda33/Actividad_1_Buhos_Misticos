b2ef9f688d71c0b2b49fd34667ad0e470a2dceb2
Barraz98128 <barrazacontreras2015@gmail.com>
2025-02-15 15:54:19 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_JoseBarraza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola a todos");
+            Console.WriteLine("Mi nombre es jose grgeoio y pertenezo al curso: Programación para Videojuegos ");
+            Console.WriteLine("escogiendo el rol: ");
+            Console.ReadLine();
+        }
+    }
+}
